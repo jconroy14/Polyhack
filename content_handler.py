e3256b353235bf582301a475c7bf76aaaeec3c97
jonathanconroy <jconroy@bancroftschool.org>
2018-10-13 12:10:39 -0400

    names

--- content_handler.py
+++ content_handler.py
@@ -65,7 +65,7 @@ def get_all_adjectives() :
 
 def get_enemy_type(level, type):
     theme = get_theme(level)
-    return get_theme_enemy_dict()[theme].split()[type]
+    return get_theme_enemy_dict()[theme][type]
 
 def get_theme_enemy_dict(): #str to str[] theme to enemies
     return create_str_dict()

2a9f8df80b94f8bf3b8c417e99fac5211225f2bc
Ryan Polhemus <ryan@polhemus.org>
2018-10-13 02:50:24 -0400

    Changed name, cleaned some

--- content_handler.py
+++ content_handler.py
@@ -1,5 +1,5 @@
-def create_dict ():
 
+def create_dict ():
     with open("adjective_book.csv", "r") as file:
         adjective_book_dict = {}
         for line in file:
@@ -11,6 +11,7 @@ def create_dict ():
                     values.append(int(word))
             adjective_book_dict[key] = values
     return adjective_book_dict
+	
 def create_str_dict(variable_file):
     with open(variable_file, "r") as file:
         str_dict = {}
@@ -20,15 +21,4 @@ def create_str_dict(variable_file):
             values = line [1]
             str_dict[key] = values 
     return str_dict
-##def main ():
-  ##  print 'start'
-    ##adjective_book_dict1 = {}
-   ## adjective_book_dict1 = create_dict()
-   ## adj_atk_descrip = create_str_dict("adj_attack.csv")
-   ## print adj_atk_descrip
-   ## for x in adj_atk_descrip:
-     ##   print(x + " " + str(adj_atk_descrip[x]))
-   ## print 'stop'
 
-##if __name__ == '__main__' :
-    main()
